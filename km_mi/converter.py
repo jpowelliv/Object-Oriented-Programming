731309e6939df21e7f0a7b239d73e6bb4c87019f
Look Alive <47085760+jpowelliv@users.noreply.github.com>
2021-07-13 15:28:14 -0500

    Km to Miles
    #Kilometers to Miles
    *Write a program that will convert kilometers to miles.

---
+++ km_mi/converter.py
@@ -0,0 +1,10 @@
+#Joseph Powell
+#CIS225-02
+#Convert Km to Mi
+
+def main():
+	miles = eval(input("How many kilometers in a mile?")
+	miles = kilometer * 0.0621371
+	print ("There are", miles, "miles.")
+	
+main()
